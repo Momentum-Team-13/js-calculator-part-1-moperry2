38fe4a6894c688186f5970c3e2bb35ef579acdd1
Michael Perry <moperrry@gmail.com>
2022-05-27 12:34:04 -0400

    got the equals button working

--- main.js
+++ main.js
@@ -2,13 +2,27 @@ console.log("test")
 let buttons = document.querySelectorAll(".button");
 console.log( buttons )
 
+let result = ""
+
+let display = document.querySelector(".display"); 
+let equalsbutton = document.querySelector("#equal")
+console.log(display)
+
 
 for (let button of buttons) {
     button.addEventListener("click", function (event) {
       console.log(event.target.textContent);
+      result += event.target.textContent
+      console.log(result)
+      display.textContent=result
     });
 }
-// let display = target.textContent
+    equalsbutton.addEventListener("click",function (){
+        const calcresult= eval(result)
+        display.textContent=calcresult
+    } );
+
+    
 
      //   event.target.classList.remove("unhighlight");
    //   event.target.classList.add("highlight");
@@ -19,10 +33,10 @@ for (let button of buttons) {
 // }
 // event.target.classList.remove("unhighlight");
 //     event.target.classList.add("highlight");
-//   });
-// function display() {
+//   });() {
 //     document.getElementById("result")
 //    }
+// function display
 
 //    function clearScreen() {
     
