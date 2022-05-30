3348c5f9c824e4fd21f499c11b479962b9f1fa6e
Michael Perry <moperrry@gmail.com>
2022-05-30 19:22:00 -0400

    cleaned up the code

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
-console.log("test")
 let buttons = document.querySelectorAll(".button");
-console.log( buttons )
-
-let result = ""
-
-let display = document.querySelector(".display"); 
-let equalsbutton = document.querySelector("#equal");
-let clearbutton = document.querySelector("#clear");
-console.log(display)
-
+let result = "";
+let display = document.querySelector(".display");
+let equalsButton = document.getElementById("equal");
+let clearButton = document.getElementById("clear");
 
 for (let button of buttons) {
-    button.addEventListener("click", function (event) {
-      console.log(event.target.textContent);
-      result += event.target.textContent
-      console.log(result)
-      display.textContent=result
-    });
+  button.addEventListener("click", function (event) {
+    result += event.target.textContent;
+    display.textContent = result;
+  });
 }
-    equalsbutton.addEventListener("click",function (){
-        const calcresult= eval(result)
-        display.textContent=calcresult
-    } );
+equalsButton.addEventListener("click", function () {
+  const calcresult = eval(result);
+  display.textContent = calcresult;
+});
 
-    clearbutton.addEventListener("click",function (){
-        event.target.id === "clear"
-        display.textContent=""
-    } );
-
-//        event.target.classList.remove("unhighlight");
-//      event.target.classList.add("highlight");
-//     });
-// }
-// function display() {
-//     document.getElementById("result").innerHTML = "7";
-// }
-// event.target.classList.remove("unhighlight");
-//     event.target.classList.add("highlight");
-//   });() {
-//     document.getElementById("result")
-//    }
-// function display
-
-//    function clearScreen() {
-    
-//    }
-//    function calculate() {
-    
-//    }
+clearButton.addEventListener("click", function () {
+  display.textContent = "";
+  result = "";
+});
